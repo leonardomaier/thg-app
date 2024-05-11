dcfb49bcb1d5e8477447661109c373f1e6d6d572
Leonardo Maier <leonarmaier@gmail.com>
2024-05-11 13:50:41 -0300

    wraps up main layout structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuItemComponent } from './components/menu/menu-item/menu-item.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuItemComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
